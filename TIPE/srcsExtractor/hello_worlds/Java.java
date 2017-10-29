633dff9637a61074aef5e3de5d8bdeaae8ca292b
rpereira <cedquen@hotmail.fr>
2017-10-29 19:48:53 +0100

    removed extra files, and save final presentation

--- TIPE/srcsExtractor/hello_worlds/Java.java
+++
@@ -1,5 +0,0 @@
-public class HelloWorld {//this file MUST have a name "HelloWorld.java"
-   public static void main(String[] args) {
-       System.out.println("Hello world!");
-   }
-}
--- TIPE/srcsExtractor/hello_worlds/PHP.php
+++
@@ -1,4 +0,0 @@
-<?php
-    echo "Hello World!";
-    //";" at the end is optional
-?>
